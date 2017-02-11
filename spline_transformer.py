e686b0badb90b538eac844b15b9bd1489d8a4396
Mehmet Kerem Turkcan <mkt2126@columbia.edu>
2017-02-10 23:54:27 -0500

    Added references.

--- spline_transformer.py
+++ spline_transformer.py
@@ -1,3 +1,9 @@
+# Spatial Spline Transformer for Keras
+#This is a port from the spatial spline transformer in Lasagne for Keras; largely resembles the original in Lasagne. Example script is attached.
+### References:
+#Original: https://github.com/Lasagne/Lasagne/blob/master/lasagne/layers/special.py#L551-L689
+#Example: https://github.com/fchollet/keras/blob/master/examples/mnist_cnn.py
+
 import theano
 import theano.tensor as T
 import numpy as np
